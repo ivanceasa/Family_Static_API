c247828d419e90b70820b96a306821817c2552cc
ivanceasa <ceaivan777@gmail.com>
2021-11-19 14:52:55 +0000

    done

--- src/app.py
+++ src/app.py
@@ -26,17 +26,39 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def create_member():
+    body = request.get_json()
+
+    member = {
+        "id": body.get("id"),
+        "first_name": body.get("first_name"),
+        "last_name": "Jackson",
+        "age": body.get("age"),
+        "lucky_numbers": body.get("lucky_numbers")
     }
+    jackson_family.add_member(member)
+    return jsonify("Member successfully added"), 200
+
+@app.route('/member/<int:id>', methods=["DELETE"])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done":True}), 200
+
+   
 
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
